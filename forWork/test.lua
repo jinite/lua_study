9774a595afc78f7552362a2436180783d592c30b
Gyujin Oh <85885544+jinite@users.noreply.github.com>
2022-11-28 17:59:20 +0900

    Update test.lua

--- forWork/test.lua
+++ forWork/test.lua
@@ -1,14 +1,28 @@
 
-JSON = (loadfile "JSON.lua")() -- one-time load of the routines
--- JSON = require("JSON")
-inspect = require("inspect")
+local JSON = require "JSON"
+local TestModule = require "test-module"
 
-local raw_json_text = "[1,2,[3,4]]"
+local old = TestModule.readFile("securitysensor-old.json")
+local new = TestModule.readFile("securitysensor-new.json")
+local oldtbl = JSON:decode(old)
+local newtbl = JSON:decode(new)
 
-local lua_value = JSON:decode(raw_json_text) -- decode example
-local raw_json_text    = JSON:encode(lua_value)        -- encode example
-local pretty_json_text = JSON:encode_pretty(lua_value) -- "pretty printed" version
+print("----------securitysensor-old.json")
+TestModule.trvtbl(oldtbl)
+print("-------------------------")
+print("----------securitysensor-new.json")
+TestModule.trvtbl(newtbl)
+print("-------------------------")
 
-print(inspect(lua_value))
-print(inspect(raw_json_text))
-print(inspect(pretty_json_text))
+local difftbl1 = TestModule.difftbl(oldtbl,newtbl,TestModule.DIFFTBL.INVISIBLE,TestModule.DIFFTBL.INVISIBLE)
+print("----------difftbl1")
+TestModule.trvtbl(difftbl1)
+print("-------------------------")
+
+
+
+
+--local raw_json_text    = JSON:encode(lua_value)        -- encode example
+--local pretty_json_text = JSON:encode_pretty(lua_value) -- "pretty printed" version
+
+--TestModule.trvtbl(lua_value)
